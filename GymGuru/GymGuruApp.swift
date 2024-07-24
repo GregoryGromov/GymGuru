f8e0327cece5cbc8cced07c1e64591fecab8b11f
gregorygromov <gregorygromow@icloud.com>
2024-07-24 14:15:02 +0300

    Initial Commit

---
+++ GymGuru/GymGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymGuruApp.swift
+//  GymGuru
+//
+//  Created by Григорий Громов on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GymGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
